b02283abf0347463b5ddf76baf60558d559da8d5
raccoonek <122537457+raccoonek@users.noreply.github.com>
2024-05-22 08:23:28 +0300

    Add files via upload

---
+++ Kurswork/Shifr_Isogenii_Kurswork/Shifr_Isogenii_Kurswork/Form_Isogenii_Kurswork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shifr_Isogenii_Kurswork
+{
+    public partial class Form_Isogenii_Kurswork : Form
+    {
+        public Form_Isogenii_Kurswork()
+        {
+            InitializeComponent();
+        }
+
+        private void button_encrypt_message_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_decrypt_message_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
